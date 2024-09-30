f80c5d05c03011222ec481e5cbb2e6abbff8f5a5
Guillermo2830 <ingguillermorojas30@gmail.com>
2024-09-30 15:31:57 -0500

    cambios

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -10,7 +10,7 @@ const products = [
       name: "Anillo Diamante",
       category: "Anillos",
       price: 450000,
-      image: '/images/anillos/anillo-diamante.webp',
+      image: '/Opulent-Joyeria/images/anillos/anillo-diamante.webp',
       description: "Anillo de diamante en oro Blanco, con acabados italianos, con un diamante en la parte superior de 30gr."
     },
     {
@@ -18,7 +18,7 @@ const products = [
       name: "Anillo",
       category: "Anillos",
       price: 250000,
-      image: '/images/anillos/anillos.jpg',
+      image: '/Opulent-Joyeria/images/anillos/anillos.jpg',
       description: "Anillo de diamante en oro Blanco, con acabados italianos, con un diamante en la parte superior."
     },
     {
@@ -26,7 +26,7 @@ const products = [
       name: "Anillo",
       category: "Anillos",
       price: 250000,
-      image: '/images/anillos/anillos.jpg',
+      image: '/Opulent-Joyeria/images/anillos/anillos.jpg',
       description: "Anillo de diamante en oro Blanco, con acabados italianos, con un diamante en la parte superior."
     },
     {
@@ -34,7 +34,7 @@ const products = [
       name: "Argolla Oro Ita",
       category: "Argollas",
       price: 200000,
-      image: '/images/argollas/argolla-1.jpg',
+      image: '/Opulent-Joyeria/images/argollas/argolla-1.jpg',
       description: "Anillo con relieve rectangular, con detalles en oro blanco."
     },
     {
@@ -42,7 +42,7 @@ const products = [
       name: "Argolla Gucci",
       category: "Argollas",
       price: 280000,
-      image: '/images/argollas/argolla-2.jpg',
+      image: '/Opulent-Joyeria/images/argollas/argolla-2.jpg',
       description: "Argollas con toque en espiral, reluciendo el perfecto estilo italiano Gucci."
     },
     {
@@ -50,7 +50,7 @@ const products = [
       name: "Argolla Real",
       category: "Argollas",
       price: 300000,
-      image: '/images/argollas/argolla-3.jpg',
+      image: '/Opulent-Joyeria/images/argollas/argolla-3.jpg',
       description: "Argolla con grabado a gusto, con detalles en Oro blanco con incrustaciones de diamante."
     },
     {
@@ -58,7 +58,7 @@ const products = [
       name: "Argolla Channel",
       category: "Argollas",
       price: 250000,
-      image: '/images/argollas/argolla-4.jpg',
+      image: '/Opulent-Joyeria/images/argollas/argolla-4.jpg',
       description: "Argollas con doble de datalle en oro, uno en oro y otro en oro blanco. Mostrando la pureza de la marca Channel."
     },
     {
@@ -66,7 +66,7 @@ const products = [
       name: "Argolla Diamantada",
       category: "Argollas",
       price: 350000,
-      image: '/images/argollas/argolla-5.jpg',
+      image: '/Opulent-Joyeria/images/argollas/argolla-5.jpg',
       description: "Argolla diamantada en la parte superior, con un Diamante de 3.5gr."
     },
     {
@@ -74,7 +74,7 @@ const products = [
       name: "Argolla Gucci",
       category: "Argollas",
       price: 300000,
-      image: '/images/argollas/argolla-6.jpg',
+      image: '/Opulent-Joyeria/images/argollas/argolla-6.jpg',
       description: "Argolla con acabados en Oro blanco, simbolo de simplicidad y exclusividad Italiana."
     },
     {
@@ -82,7 +82,7 @@ const products = [
       name: "Cadena Virgen",
       category: "Cadenas",
       price: 350000,
-      image: '/images/cadenas/cadena-cristo-2.jpg',
+      image: '/Opulent-Joyeria/images/cadenas/cadena-cristo-2.jpg',
       description: "Cadena con retrato de la Virgen en Oro, perfecta para brindar un hermoso recuerdo a la madre de Jesus."
     },
     {
@@ -90,7 +90,7 @@ const products = [
       name: "Cadena Jesucristo",
       category: "Cadenas",
       price: 250000,
-      image: '/images/cadenas/cadena-cristo.jpg',
+      image: '/Opulent-Joyeria/images/cadenas/cadena-cristo.jpg',
       description: "Cadena con retrato de Jesucristo en Oro, con un mallado en toda la cadena superior."
     },
     {
@@ -98,7 +98,7 @@ const products = [
       name: "Cadena Rolex",
       category: "Cadenas",
       price: 400000,
-      image: '/images/cadenas/cadena-lx.jpg',
+      image: '/Opulent-Joyeria/images/cadenas/cadena-lx.jpg',
       description: "Cadena con emblema Rolex totalmente en Oro."
     },
     {
@@ -106,7 +106,7 @@ const products = [
       name: "Cadena Rolex Diamantada",
       category: "Cadenas",
       price: 250000,
-      image: '/images/cadenas/cadena-rolex.jpg',
+      image: '/Opulent-Joyeria/images/cadenas/cadena-rolex.jpg',
       description: "Cadena con emblema Rolex Diamantada completamente, luce exclusivo y lujoso."
     },
     {
@@ -114,7 +114,7 @@ const products = [
       name: "Rosario Oro Italiano",
       category: "Cadenas",
       price: 500000,
-      image: '/images/cadenas/rosario-2.jpg',
+      image: '/Opulent-Joyeria/images/cadenas/rosario-2.jpg',
       description: "Cadena de rosario, compuesta totalmente de Oro y Oro blanco."
     },
     {
@@ -122,7 +122,7 @@ const products = [
       name: "Rosario Oro y Plata",
       category: "Cadenas",
       price: 250000,
-      image: '/images/cadenas/rosario-3.jpg',
+      image: '/Opulent-Joyeria/images/cadenas/rosario-3.jpg',
       description: "Rosario con perlas en Oro y Oro Blanco alrededor de todo el rosario."
     },
     {
@@ -130,7 +130,7 @@ const products = [
       name: "Rosario Oro",
       category: "Cadenas",
       price: 250000,
-      image: '/images/cadenas/rosario.jpg',
+      image: '/Opulent-Joyeria/images/cadenas/rosario.jpg',
       description: "Rosario en Oro en toda su superfice, con retrato de jesucristo."
     },
     {
@@ -138,7 +138,7 @@ const products = [
       name: "Esclava Oro",
       category: "Esclavas",
       price: 250000,
-      image: '/images/esclavas/esclava-1.jpg',
+      image: '/Opulent-Joyeria/images/esclavas/esclava-1.jpg',
       description: "Esclava en Oro con relieve de escamas en toda su superficie."
     },
     {
@@ -146,7 +146,7 @@ const products = [
       name: "Esclava Gucci",
       category: "Esclavas",
       price: 250000,
-      image: '/images/esclavas/esclava-2.jpg',
+      image: '/Opulent-Joyeria/images/esclavas/esclava-2.jpg',
       description: "Esclava Gucci con Oro en totalidad."
     },
     {
@@ -154,7 +154,7 @@ const products = [
       name: "Esclava Rolex",
       category: "Esclavas",
       price: 250000,
-      image: '/images/esclavas/esclava-3.jpg',
+      image: '/Opulent-Joyeria/images/esclavas/esclava-3.jpg',
       description: "Esclava rolex con Oro en su totalidad."
     },
     {
@@ -162,7 +162,7 @@ const products = [
       name: "Esclava Oro y Plata",
       category: "Esclavas",
       price: 250000,
-      image: '/images/esclavas/esclava-4.jpg',
+      image: '/Opulent-Joyeria/images/esclavas/esclava-4.jpg',
       description: "Esclava adornada con Oro y Oro blanco dandole un diseño exclusivo y lujoso a tu muñeca."
     },
     {
@@ -170,7 +170,7 @@ const products = [
       name: "Esclava Diamantada",
       category: "Esclavas",
       price: 250000,
-      image: '/images/esclavas/esclava-5.jpg',
+      image: '/Opulent-Joyeria/images/esclavas/esclava-5.jpg',
       description: "Esclava dimantada en toda su superficie, con un diseño ovalado en todas sus intersecciones."
     },
   ]
